717336e984f042b625829af0abeab4b52a386949
Maksym Ivanov <maksym.ivanov@zoominsoftware.com>
2025-02-21 17:06:13 +0100

    test: switch away from netcat; impl 2 last steps; tighten timeouts

---
+++ test/web-hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import http.server
+import os,sys
+import socketserver
+from http import HTTPStatus
+from time import sleep
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        body = f"Hello from PID {os.getpid()} listening on port {listenport} !\nThis is dummy app.\n"
+        self.wfile.write(body.encode('ascii'))
+
+listenport = int(os.getenv('PORT', 8000))
+startdelay = float(os.getenv('DELAY', 5))
+willcrash = os.getenv('DO_CRASH') is not None
+
+print(f"Dummy app starting up! DELAY={startdelay}, DO_CRASH={willcrash}")
+sleep(startdelay)
+
+if willcrash:
+    print("Oops!")
+    sys.exit(1)
+
+print(f"Listening on port {listenport}")
+httpd = socketserver.TCPServer(('', listenport), Handler)
+httpd.serve_forever()
